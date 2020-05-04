fe1ecad6b8fae6fc3d383b0f234bbea7852268a3
arijsk <arijsakhraoui18@gmail.com>
2020-05-04 11:04:01 +0200

    First commit pour le TP

---
+++ src/app/article-creation/article-creation.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../services/article.service';
+import { NgForm } from '@angular/forms';
+import { Article } from '../models/article';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-article-creation',
+  templateUrl: './article-creation.component.html',
+  styleUrls: ['./article-creation.component.css']
+})
+export class ArticleCreationComponent implements OnInit {
+
+  newArticle: Article = {
+    id: null,
+    title: '',
+    content: '',
+    authors: ''
+  };
+
+
+  constructor(private articleService: ArticleService, private router: Router) { }
+
+  onSubmitNewArticle(creationForm: NgForm) {
+    if (creationForm.valid) {
+      this.articleService.post(this.newArticle).subscribe(
+        (result) => {
+          console.log(result);
+          this.router.navigate(['/articles']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
